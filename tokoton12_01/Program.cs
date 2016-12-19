8a172ebee10899be74e93f2e88d1dece7dea684c
kobayashi tomoaki <t.koba0330@hotmail.co.jp>
2016-12-19 23:57:01 +0900

    tokoton C# 12章

---
+++ tokoton12_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tokoton12_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread((x) =>
+            {
+                for (;;)
+                {
+                    Console.WriteLine(DateTime.Now.Second);
+                    Thread.Sleep(1000);
+                }
+            });
+
+            Console.WriteLine("Type Enter Key to stop!");
+            t.Start();
+
+            Console.WriteLine(Console.ReadLine());
+            t.Abort();
+        }
+    }
+}
